ec6c42af430ce1f5e1f6462817c67b8f8bc92edc
rei-kaizen <111290781+rei-kaizen@users.noreply.github.com>
2023-04-27 16:20:59 +0800

    Add border decor to each quote in file

--- text-input-capture.py
+++ text-input-capture.py
@@ -3,15 +3,22 @@ file = open("mylife.txt", "w" )
 
 #use a while loop to keep asking for user text input and store in file until stopped
 while True:
-    ##ask for user input
+    #ask for user input
     text = input("Enter line: ")
-    ##write it over to the file
-    file.write(text + "\n")
-    ##if there are more line of text to input, type y for yes/continue and n for no/stop
+    #add quotation mark
+    quote = f' "{text}" '
+    #create borders around the quote
+    top_border = "====" * 10 + "\n"
+    bottom_border = "\n" + "====" * 10 + "\n"
+    frame = top_border + quote + bottom_border
+    #write it over to the file
+    file.write(frame)
+    #if there are more line of text to input, type y for yes/continue and n for no/stop
     option = input("Are there more lines (y/n)? ")
-    ##continue the loop until the user terminate the loop
+    #continue the loop until the user terminate the loop
     if option.lower() == "n":
         break 
 
 #file close
 file.close() 
+
