96af6612ccb3437a3a3a4a5575b4cbc714dd1a68
rei-kaizen <111290781+rei-kaizen@users.noreply.github.com>
2023-04-27 16:10:30 +0800

    continuosly prompt user input and store in file

--- text-input-capture.py
+++ text-input-capture.py
@@ -1,12 +1,17 @@
 #open mylife.txt file with the write method
 file = open("mylife.txt", "w" )
-text = input("Enter line: ")
-file.write(text)
-file.close()
 
 #use a while loop to keep asking for user text input and store in file until stopped
-##ask for user input
-##write it over to the file
-##if there are more line of text to input, tyoe y for yes/continue and n for no/stop
-##continue the loop until the user terminate the loop
-#file close 
+while True:
+    ##ask for user input
+    text = input("Enter line: ")
+    ##write it over to the file
+    file.write(text + "\n")
+    ##if there are more line of text to input, type y for yes/continue and n for no/stop
+    option = input("Are there more lines (y/n)? ")
+    ##continue the loop until the user terminate the loop
+    if option.lower() == "n":
+        break 
+
+#file close
+file.close() 
